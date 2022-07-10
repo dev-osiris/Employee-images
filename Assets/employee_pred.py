045c07db4d1f4476395736c18851a352055a99d1
Livesh kumar <57588397+dev-osiris@users.noreply.github.com>
2022-07-10 14:33:17 +0530

    generates prediction using user fed input data
    This file consumes model.pkl and scalar.pkl produced by main.py and uses them to make a prediction which is sent back to flask_app.py

--- Assets/employee_pred.py
+++ Assets/employee_pred.py
@@ -1,34 +1,19 @@
 
 import pickle
 
-# de-serialize mlp_nn.pkl file into an object called mlp_nn using pickle
-
+# de-serialize model.pkl file into an object called mdl using pickle
 with open('model.pkl', 'rb') as handle:
     mdl = pickle.load(handle)
 
+# de-serialize scalar.pkl file into an object called scl using pickle
 with open('scalar.pkl', 'rb') as handle:
     scl = pickle.load(handle)
 
+# scale the user fed data
 def scale(sample):
     return scl.transform(sample)
 
+# make predicion
 def predict(sample):
     sample_scaled = scale(sample)
     return mdl.predict(sample_scaled)
-
-# no we can call various methods over mlp_nn as as:
-# Let X_test be the feature (UNIX timestamp) for which we want to predict the output
-# sample = np.array([[8,  # department code
-#            2,  # masters degree
-#            0,  # male
-#            1,  # 1 training
-#            57,  # 30 years old
-#            1,  # previous year rating
-#            29,  # length of service
-#            0,  # KPIs met >80%
-#            0,  # awards won
-#            81,  # avg training score
-#            1,  # sum of metric
-#            81  # total score
-#            ]])
-
